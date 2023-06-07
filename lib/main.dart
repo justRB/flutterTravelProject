b6965aa4419ba89c0bb374140ca9cb5b4ba88935
justRB <bietrix.romuald@gmail.com>
2023-06-07 13:36:39 +0200

    welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,17 @@
-import 'package:country_state_city_picker/country_state_city_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_project/ui/screens/welcome.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Country State and City Picker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late String countryValue;
-  late String cityValue;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Country State and City Picker'),
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          height: 600,
-          child: Column(
-            children: [
-              SelectState(
-                onCountryChanged: (value) {
-                  setState(() {
-                    countryValue = value;
-                  });
-                },
-                onCityChanged: (value) {
-                  setState(() {
-                    cityValue = value;
-                  });
-                },
-                onStateChanged: (String value) {},
-              ),
-              InkWell(
-                  onTap: () {
-                    print('country selected is $countryValue');
-                    print('country selected is $cityValue');
-                  },
-                  child: Text(' Check'))
-            ],
-          )),
+    return const MaterialApp(
+      home: Welcome(),
     );
   }
 }
