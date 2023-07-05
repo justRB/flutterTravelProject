58d68311f79df1ddc24dc12ea80192ab6288c647
justRB <bietrix.romuald@gmail.com>
2023-07-05 09:50:56 +0200

    filter and  publication buttons

--- lib/ui/screens/city.dart
+++ lib/ui/screens/city.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_project/ui/widgets/publication_card.dart';
-
 import '../../models/city_class.dart';
 import '../widgets/header.dart';
 
@@ -12,6 +11,20 @@ class City extends StatefulWidget {
 }
 
 class _CityState extends State<City> {
+  final List<String> filterList = <String>[
+    'Plus réçents',
+    'Plus anciens',
+    'Mieux notés',
+    'Moins bien notés'
+  ];
+  late String itemSelected;
+
+  @override
+  void initState() {
+    super.initState();
+    itemSelected = filterList[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     CityClass cityObject =
@@ -22,55 +35,119 @@ class _CityState extends State<City> {
         showProfile: true,
         showReturn: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              cityObject.name,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
               children: [
                 Text(
-                  cityObject.score.toString(),
+                  cityObject.name,
                   style: const TextStyle(
-                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
                   ),
                 ),
-                const Icon(
-                  Icons.star,
-                  color: Color.fromARGB(255, 255, 209, 59),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      cityObject.score.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Icon(
+                      Icons.star,
+                      color: Color.fromARGB(255, 255, 209, 59),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return SingleChildScrollView(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 25, right: 25),
+                          child: const PublicationCard(
+                            authorFirstname: 'Fabien',
+                            authorLastName: 'Dubois',
+                            imageUrl: 'paris.jpg',
+                            comment:
+                                'La Tour Eiffel de jour sur Paris est magnifique, il fait chaud avec un super soleil, en famille nous continuons notre séjour là-bas. La Tour Eiffel de jour sur Paris est magnifique, il fait chaud avec un super soleil, en famille nous continuons notre séjour là-bas.',
+                            date: '04/07/2023 - 13:59',
+                            score: 4.3,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
-            Expanded(
-              child: PageView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return SingleChildScrollView(
+          ),
+          Positioned(
+            bottom: 25,
+            left: 0,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 50,
+                    padding: const EdgeInsets.only(left: 10, right: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: DropdownButton<String>(
+                      underline: const SizedBox(),
+                      value: itemSelected,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          itemSelected = newValue!;
+                        });
+                      },
+                      items: filterList.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  GestureDetector(
                     child: Container(
-                      margin: const EdgeInsets.only(left: 25, right: 25),
-                      child: const PublicationCard(
-                        authorFirstname: 'Fabien',
-                        authorLastName: 'Dubois',
-                        imageUrl: 'paris.jpg',
-                        comment:
-                            '123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 ',
-                        date: '04/07/2023 - 13:59',
-                        score: 4.3,
+                      padding: const EdgeInsets.only(left: 10, right: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      width: MediaQuery.of(context).size.width / 2.5,
+                      height: 50,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Publier un avis'),
+                          Icon(Icons.add),
+                        ],
                       ),
                     ),
-                  );
-                },
+                    onTap: () {},
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
