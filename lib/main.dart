2056609b606f3fb4e1da89b22ee6f95af58a2b29
maxence-lh <lh.maxence@live.fr>
2023-07-05 10:11:18 +0200

    ajout accès Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_project/ui/screens/welcome.dart';
+
 import 'ui/screens/city.dart';
 import 'ui/screens/profile.dart';
 
 Future<void> main() async {
+  await Firebase.initializeApp();
   await initialization(null);
 
   runApp(const MyApp());
