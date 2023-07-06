1ee6cbd4386383d7cf3c5892a15b89a0960159a8
maxence-lh <lh.maxence@live.fr>
2023-07-06 14:59:49 +0200

    publication page

--- lib/ui/screens/publication.dart
+++ lib/ui/screens/publication.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../widgets/header.dart';
 
@@ -10,9 +13,10 @@ class Publication extends StatefulWidget {
 }
 
 class _PublicationState extends State<Publication> {
+  File? _selectedImage;
   double _sliderValue = 0.0;
-  TextEditingController _textController = TextEditingController();
-  int _maxLength = 400;
+  final TextEditingController _textController = TextEditingController();
+  final int _maxLength = 400;
 
   @override
   Widget build(BuildContext context) {
@@ -23,51 +27,96 @@ class _PublicationState extends State<Publication> {
         showProfile: true,
         showReturn: true,
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const Text("Séléctionnez une image"),
-            const Text("Donnez une note"),
-            Slider(
-              value: _sliderValue,
-              min: 0.0,
-              max: 5.0,
-              divisions: 50,
-              label: _sliderValue.toStringAsFixed(1),
-              activeColor: const Color.fromARGB(255, 213, 92, 6),
-              thumbColor: const Color.fromARGB(255, 213, 184, 6),
-              onChanged: (newValue) {
-                setState(() {
-                  _sliderValue = newValue;
-                });
-              },
-            ),
-            const Icon(
-              Icons.star,
-              size: 50.0,
-              color: Colors.amber,
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 35),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.white,
-              ),
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: TextField(
-                controller: _textController,
-                maxLength: _maxLength,
-                maxLines: null,
-                style:
-                    const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                decoration: InputDecoration(
-                  labelText: 'Commentaire (maximum $_maxLength characters)',
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: _selectedImage != null
+                      ? DecorationImage(
+                          image: FileImage(_selectedImage!),
+                          fit: BoxFit.cover,
+                        )
+                      : null,
                 ),
               ),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: _pickImageFromGallery,
+                child: Text('Choisir une photo'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 35),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.white,
+                ),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Column(
+                  children: [
+                    const Text('Donnez une note'),
+                    Slider(
+                      value: _sliderValue,
+                      min: 0.0,
+                      max: 5.0,
+                      divisions: 50,
+                      label: _sliderValue.toStringAsFixed(1),
+                      activeColor: const Color.fromARGB(255, 213, 92, 6),
+                      thumbColor: const Color.fromARGB(255, 213, 184, 6),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _sliderValue = newValue;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 35),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.white,
+                ),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _textController,
+                    maxLength: _maxLength,
+                    maxLines: null,
+                    decoration: const InputDecoration(
+                      labelText: 'Ecrivez votre commentaire',
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 25, bottom: 35),
+                child: FloatingActionButton.extended(
+                    label: const Text("Publiez"),
+                    backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                    icon: const Icon(Icons.send),
+                    onPressed: () {}),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _pickImageFromGallery() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
   }
 }
